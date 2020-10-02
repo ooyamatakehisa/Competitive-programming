e8b8a3c7282e0203824dd38a8a367f025023712c
ooyamatakehisa <takehisa.oyama110656@e-seikatsu.co.jp>
2020-10-02 21:14:06 +0900

    ABC167D

--- ABC167/d.cpp
+++ ABC167/d.cpp
@@ -13,5 +13,30 @@ using namespace std;
 typedef long long ll;
 
 int main(){
+    int n;
+    ll k;
+    cin >> n >> k;
+    vector<int> a(n);
+    vector<pair<int, int>> f(n, pair<int, int>(0, 0));
+    FOR (i, 0, n) {
+        cin >> a[i]; a[i]--;
+    }
+
+    int next = 0, cnt = 0;
+    f[0].first = 1;
+    
+    while (true) {
+        next = a[next]; cnt++;
+        if (f[next].first) break;
+        f[next].first = 1;
+        f[next].second = cnt;
+    }
+    int si = f[next].second;
+    int T = cnt - si;
+
+    int step = (k < n) ? k : si + (k - si) % T;
+    int ans = 0;
+    FOR (ki, 0, step) ans = a[ans];
+    cout << ans + 1  << endl;
     return 0;
 }
